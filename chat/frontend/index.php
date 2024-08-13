fc904e35f82d010cc0dbdd70b978678bdfb4d7b4
Draky <volkovgrisha80@gmail.com>
2024-08-13 16:16:23 +0300

    добавиви класів для внесення стилів

--- chat/frontend/index.php
+++ chat/frontend/index.php
@@ -4,36 +4,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Головна сторінка</title>
+    <link rel="stylesheet"href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
         <link rel="stylesheet" href="css/style.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <link rel="stylesheet" href=" https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
     <body>
     <header>
-            <nav>
-                <a>
-                <img src="../photo/Без%20названия.png" width="224" height="224"
-                     alt="Логотип мого сайту">
-                </a>
-                <ul>
-                    <li><a href="index.php">Головна</a></li>
-                    <li><a href="news.php">Новини</a></li>
-                    <li><a href="shop.php">Магазин</a></li>
-                    <li><a href="contact.php">Контакти</a></li>
-                    <li><a href="/chat/chat.php">Чат</a></li>
-                </ul>
-                <ul>
-                    <li>
-                        <button type="button"><a href="/chat/login.php">Вхід</a></button>
-                    </li>
-                </ul>
-            </nav>
+        <nav class="navigation-fon">
+            <div class="nav-links">
+                <a class="navigation" href="index.php">Головна</a>
+                <a class="navigation" href="news.php">Новини</a>
+                <a class="navigation" href="shop.php">Магазин</a>
+                <a class="navigation" href="contact.php">Контакти</a>
+                <a class="navigation" href="/chat/chat.php">Чат</a>
+            </div>
+            <a class="navigation login" href="/chat/login.php">Вхід</a>
+        </nav>
+<!--        <a>-->
+<!--            <img src="../photo/Без%20названия.png" width="224" height="260"-->
+<!--                 alt="Логотип мого сайту">-->
+<!--        </a>-->
     </header>
         <main>
-ТЕСТ
        <h1>Заголовок</h1>
-
-       <section>
+       <section class="advantages">
            <h2>Переваги</h2>
            <ul>
                <li>
@@ -50,21 +45,21 @@
                </li>
            </ul>
        </section>
-       <section>
+       <section class="news">
            <h2> Новини про нашу багадельню</h2>
            <ul>
                <li>
-                   <p>Я не з такої сім'ї як всі я з Богатої</p>
-                   <time datetime="2024-08-05">5 серпня</time>
+                   <p class="news-text">Я не з такої сім'ї як всі я з Богатої</p>
+                   <time class="news-date" datetime="2024-08-05">5 серпня</time>
                </li>
                <li>
-                   <p>Я не з такої сім'ї як всі я з Богатої</p>
-                   <time datetime="2024-08-07">7 серпня</time>
+                   <p class="news-text">Я не з такої сім'ї як всі я з Богатої</p>
+                   <time class="news-date" datetime="2024-08-07">7 серпня</time>
                </li>
            </ul>
            <a href="news.php">Всі новини</a>
        </section>
-       <section>
+       <section class="gallery">
            <h2>Фотогалерея</h2>
            <figure>
                <a href="#">
@@ -72,10 +67,10 @@
                      alt="Гори">
                </a>
            </figure>
-               <button type="button">назад</button>
-               <button type="button">вперед</button>
+               <button class="bumbutton" type="button">Попередня</button>
+               <button class="bumbutton" type="button">Наступна</button>
        </section>
-       <section>
+       <section class="contacts">
            <h2>Контактна інформація</h2>
            <p>Перукарня у Ярка<br>
                Адреса, Гуртах 4 кімната 11 блок вулиця Тарнавського 7<br>
@@ -83,22 +78,40 @@
            </p>
            <p>
                Час роботи<br>
-               пн-пт: з 10:00 до 22:00<br>
-               сб-нд: з 10:00 до 19:00
+               Пн-пт: з 10:00 до 22:00<br>
+               Сб-нд: з 10:00 до 19:00
            </p>
-           <a href="#">як нас знайти</a><br>
-           <a href="contact.php">наші контакти</a>
+           <a href="#">Як нас знайти</a><br>
+           <a href="contact.php">Наші контакти</a>
        </section>
-            <section>
+            <section class="notation">
                 <h2>Запис</h2>
                 <p>Вкажіть бажану дату і час і ми звяжемося з вами для затвердження </p>
-<!--                Форму потім зроблю-->
+                <form action="///" method="post">
+                    <p>
+                        <label for="appointment-date">Дата</label>
+                        <input id="appointment-date" tupe="date" name="date" value="2024-08-13">
+                    </p>
+                    <p>
+                        <label for="appointment-time">Час</label>
+                        <input id="appointment-time" tupe="time" name="time" value="10:00">
+                    </p>
+                    <p>
+                        <label for="appointment-name">Ваше імя</label>
+                        <input id="appointment-name" tupe="text" name="name" value="" placeholder="борода">
+                    </p>
+                    <p>
+                        <label for="appointment-phone">Телефон</label>
+                        <input id="appointment-phone" tupe="tel" name="tel" value="" placeholder="+380845734665">
+                     </p>
+                    <button type="submit">Відправити</button>
+                </form>
             </section>
         </main>
-       <footer>
+       <footer class="footer">
            <p>Перукарня у Ярка<br>
                Адреса, Гуртах 4 кімната 11 блок вулиця Тарнавського 7<br>
-               <a href="#">як нас знайти</a><br>
+               <a href="#">Як нас знайти</a><br>
                Телефон +380660948329</p>
            <div>
                <p>соц мережі</p>
@@ -109,20 +122,12 @@
                </ul>
            </div>
            <p>
-               Розробленно:
+               Розроблено:
                <a href="https://t.me/gishavolkov"><i class="fa-brands fa-telegram" style="color: #74C0FC;">Grisha</i></a></li>
            </p>
        </footer>
-
-    <section>
-        <h2>Власний кабінет</h2>
-        <p>Введіть логін і пароль</p>
-<!--        Форма-->
-        <button type="button">закрити</button>
-    </section>
-
-    <section>
-        <h2> як нас знайти</h2>
+    <section class="map">
+        <h2> Як нас знайти</h2>
         <button type="button">закрити</button>
     </section>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
