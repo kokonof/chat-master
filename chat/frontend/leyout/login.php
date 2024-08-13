d99e4b94e57550c125eb9b388163194dca584d40
Draky <volkovgrisha80@gmail.com>
2024-08-13 16:14:56 +0300

    добавив одну колонку

--- chat/frontend/leyout/login.php
+++ chat/frontend/leyout/login.php
@@ -1,4 +1,4 @@
-<div class="">
+
     <div class="overlay"></div>
     <div class="container pt-5">
         <h2 class="text-center mb-4 text-white">Вхід у систему</h2>
@@ -7,12 +7,15 @@
                 <label for="email"><i class="fas fa-envelope"></i> Email:</label>
                 <input type="email" id="email" name="email" class="form-control" placeholder="Введіть email" required>
             </div>
-            <div class="form-group">
+            <div class="form-group valera">
                 <label for="password"><i class="fas fa-lock"></i> Пароль:</label>
-                <input type="password" id="password" name="password" class="form-control" placeholder="Введіть пароль" required>
+                <input type="password" id="password" name="password" class="form-control" placeholder="Введіть пароль" required><br>
+
+                <input type="checkbox" name="remember" id="login-password-info">
+                <label for="login-password-info">Запамятайте мене</label>
+                <a href="#">Я забув пароль</a>
             </div>
             <button type="submit" class="btn btn-primary btn-block"><i class="fas fa-sign-in-alt"></i> Увійти</button>
         </form>
         <p class="text-center mt-3 text-white">Не маєте акаунту? <a href="../../register.php" class="text-white"><strong>Зареєструйтесь</strong></a></p>
     </div>
-</div>
