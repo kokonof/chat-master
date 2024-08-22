98c0b16f8ae2e080cb54cb815bfac967f1543fe1
Draky <volkovgrisha80@gmail.com>
2024-08-22 14:34:16 +0300

    дописав головну сторінку і стилі

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+// Якщо це пре-запит (preflight request)
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    exit(0); // Припиняємо виконання для preflight запиту
+}
 require_once './init.php';
 
 $url = isset($_GET['url']) ? explode('/', filter_var(rtrim($_GET['url'], '/'), FILTER_SANITIZE_URL)) : [];
--- src/controllers/MainController.php
+++ src/controllers/MainController.php
@@ -10,7 +10,16 @@ class MainController {
         $users = $userModel->getMessages();
 
         $template = new Template();
-//        $template->setLayout('main');
+//        $template->setL ayout('main');
         $template->render(compact('users'),'main/index');
+    }
+
+    public function getMessagesJson() {
+        $userModel = new Chat();
+        $messages = $userModel->getMessages();
+//        var_dump($messages);
+
+        header('Content-Type: application/json');
+        echo json_encode($messages);
     }
 }
