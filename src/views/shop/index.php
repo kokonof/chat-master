6ff6dca11546943216957b57c20557144e4c11d2
Draky <volkovgrisha80@gmail.com>
2024-08-29 12:55:46 +0300

    добавив контент на сторінку магазину

--- src/views/shop/index.php
+++ src/views/shop/index.php
@@ -1,1 +1,58 @@
-<?php
+<main class="container">
+
+    <div class="inner-page-container">
+        <h1>Прайс-лист</h1>
+        <ul class="breadcrumbs">
+            <li><a href="index.php">Головна</a></li>
+            <li class="breadcrumbs-current">Прайс-лист</li>
+        </ul>
+    </div>
+    <section class="inner-content">
+        <h2 class="big-heading">Тільки чоловіча стрижка</h2>
+
+        <div class="inner-columns">
+            <div class="inner-columns-left">
+
+                <h3>Ми використовуємо тільки<br> Найкращі засоби:</h3>
+                <ul class="custom-list-1">
+                    <li>Baxter of California</li>
+                    <li>Mr natty</li>
+                    <li>Suavecito</li>
+                    <li>Malin+Goetz</li>
+                </ul>
+            </div>
+            <div class="inner-columns-right">
+
+                <h3>Ціни на послуги <br>Наших майстрів:</h3>
+                <table class="custom-table-1">
+                    <tr>
+                        <td>Стрижка</td>
+                        <td>1500$</td>
+                    </tr>
+                    <tr>
+                        <td>Стрижка бороди</td>
+                        <td>500$</td>
+                    </tr>
+                    <tr>
+                        <td>Стрижка вусів</td>
+                        <td>350$</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+        <div class="inner-columns">
+                <h3>Декілька слів про нас</h3>
+            <div class="inner-columns-left">
+                <p>
+                    "Ми прагнемо до інновацій та якості в кожному аспекті нашої роботи."
+                </p>
+
+            </div>
+            <div class="inner-columns-right">
+                <p>
+                    "Досвід і надійність, які формують майбутнє вашого успіху."
+                </p>
+            </div>
+        </div>
+    </section>
+</main>
