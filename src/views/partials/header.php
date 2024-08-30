3fea30011c53008e51ce53596996b80abebbde91
Draky <volkovgrisha80@gmail.com>
2024-08-30 14:15:09 +0300

    замінив ссилку

--- src/views/partials/header.php
+++ src/views/partials/header.php
@@ -8,7 +8,6 @@
                     alt="Логотип мого сайту" height="44" width="44px"></a>';
                 }
              ?>
-
             <a class="navigation" href="<?php echo BASE_URL; ?>">Головна</a>
             <a class="navigation" href="<?php echo BASE_URL; ?>chat">Чат</a>
             <a class="navigation" href="<?php echo BASE_URL; ?>news">Новини</a>
@@ -16,9 +15,10 @@
             <a class="navigation" href="<?php echo BASE_URL; ?>contact">Контакти</a>
         </div>
         <div class="nav-links right-menu">
-            <a class="navigation-login navigation " href="<?php echo BASE_URL; ?>news">Вхід_2</a>
-            <a class="navigation-login navigation " href="<?php echo BASE_URL; ?>auth/login">Вхід</a>
+            <button class="navigation-login navigation_2">Вхід_2</button>
+            <a class="navigation-login navigation_1 " href="<?php echo BASE_URL; ?>auth/login">Вхід</a>
             <a class="navigation navigation-login" href="<?php echo BASE_URL; ?>auth/register">Реєстрація</a>
         </div>
     </nav>
 </header>
+<?php require_once BASE_PATH . 'views/partials/modal-login.php'; ?>
